aa93a3df29b09fa77930c8828243c4abf11580f3
veerabhadrahareesh <114908098+veerabhadrahareesh@users.noreply.github.com>
2023-07-12 12:37:24 +0530

    1.TrainList.js

--- Folder a"/TrainList.js
+++ Folder a"/TrainList.js
@@ -1,3 +1,15 @@
+QUESTION- Develop an API to display all trains in the next 12 hours with their seat availability & pricing.
+
+As a developer, you have received pilot access to the APIs of John Doe Railways. Using the APIs provided by the server, develop an API that displays the real time train schedule of all trains in the next 12 hours
+
+Display real-time train schedules of all trains along with seat availability, prices User shouldn't be asked to register to view the train schedules from your server
+
+You are free to use any backend framework of your choice.
+
+Each train contains seat availability and prices for 2 train coach types-sleeper and AC • Trains departing in the next 30 minutes should be ignored
+
+
+CODE-
 import React ,{useEffect, useState }from 'react';
 const TrainList = () => {
   const [trains, setTrains] = useState([]);
